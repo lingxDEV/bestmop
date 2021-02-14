2eb1c8f8ed0d50f1343abbf511fb22b1fef571f8
walter <elmaster.n1@gmail.com>
2021-02-14 04:16:34 -0300

    main

---
+++ sql/Update_2021/Creature [34980] Drottinn Hrothgar .sql
@@ -0,0 +1,91 @@
+/*
+SQL ZONAXTREMA 				  
+Desarrollado por:
+Fecha: 23/1/2021
+### Scripts Trabajados por ZONAXTREMA  ########
+### Datos SQL ###########################################*/
+SET @CREATURE  	:= 'Drottinn Hrothgar';
+SET @ENTRY 		:= '34980';
+/*SET @MAP   		:= 530;       *//*(Outland)*/
+/*SET @ZONE  		:= 6455;      *//*(Sunstrider Isle)*/
+/*SET @AREA  		:= 3431;      *//*(Sunstrider Isle)*/
+/*#######################################################*/
+
+
+/* Step 1.1 Table  `creature_template` */
+DELETE FROM `creature_template` WHERE `entry` = @ENTRY;
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `difficulty_entry_4`, `difficulty_entry_5`, `difficulty_entry_6`, `difficulty_entry_7`, `difficulty_entry_8`, `difficulty_entry_9`, `difficulty_entry_10`, `difficulty_entry_11`, `difficulty_entry_12`, `difficulty_entry_13`, `difficulty_entry_14`, `difficulty_entry_15`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `exp_unk`, `faction_A`, `faction_H`, `npcflag`, `npcflag2`, `speed_walk`, `speed_run`, `speed_swim`, `speed_fly`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `type_flags2`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `Health_mod`, `Mana_mod`, `Mana_mod_extra`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES 
+(@ENTRY, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '29504', '0', '0', '0', @CREATURE, 'The Sea Reaver', '', '0', '79', '80', '2', '0', '1885', '1885', '0', '0', '1', '1.14286', '0', '1.14286', '1', '0', '422', '586', '0', '642', '1', '2000', '2000', '1', '33280', '2048', '0', '0', '0', '0', '0', '0', '345', '509', '103', '7', '2147483648', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'SmartAI', '0', '3', '1', '1.1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '', '16048');
+
+/* Step 1.3 Table  creature_template_addon */
+DELETE FROM creature_template_addon WHERE  `entry` = @ENTRY;
+/* Step 2.7 Table  `creature_model_info */
+DELETE m1 FROM creature_template AS ct JOIN creature_model_info AS m1 ON ct.modelid1 = m1.modelid OR ct.modelid2 = m1.modelid OR ct.modelid3 = m1.modelid OR ct.modelid4 = m1.modelid WHERE entry = @ENTRY;
+INSERT INTO `creature_model_info` (`modelid`,`bounding_radius`,`combat_reach`,`gender`,`modelid_other_gender`) VALUES 
+('29504', '0.408', '1.8', '0', '0'); 
+
+
+/* Step 1.6 Table  `creature_text */
+DELETE FROM  `creature_text` WHERE `entry`= @ENTRY;
+/* Step 1.6 Table  `smart_scripts` */
+DELETE FROM smart_scripts WHERE `entryorguid` = 34980 AND `source_type`= 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+('34980', '0', '0', '0', '0', '0', '100', '0', '5000', '5700', '9800', '13600', '11', '15496', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', 'Cast Cleave'), 
+('34980', '0', '1', '0', '0', '0', '100', '0', '9900', '10000', '17800', '24500', '11', '67038', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', 'Cast Smash'); 
+
+
+/* Step 1.7 Table  `creature */
+DELETE FROM creature WHERE  `id` = @ENTRY;
+/*## Step 2: Creature Loots ##########################################*/
+
+/* Step 2.2 Table  `creature_loot_currency */
+DELETE FROM creature_loot_currency WHERE  `creature_id` = @ENTRY;
+/* Step 2.1 Table  `creature_onkill_reputation */
+DELETE FROM creature_onkill_reputation WHERE `creature_id` = @ENTRY;
+/* Step 2.3 Table  `pickpocketing_loot_template */
+DELETE FROM pickpocketing_loot_template WHERE `entry` = @ENTRY;
+/* Step 2.4 Table  `skinning_loot_template */
+DELETE FROM skinning_loot_template WHERE  `entry` = @ENTRY;
+/*Creature Loot Template Items Data: Id - Quality - SellPrice - ItemLevel - RequiredLevel - Name */
+/*
+/*Pickpocketing Loot Template Items Data: Id - Quality - SellPrice - ItemLevel - RequiredLevel - Name */
+/*
+*/
+/*Skinning Loot Template Items Data: Id - Quality - SellPrice - ItemLevel - RequiredLevel - Name */
+/*
+*/
+
+
+/*## Step 3: Creature Extras ##########################################*/
+
+/* Step 3.3 Table  `creature_summon_groups` */
+DELETE FROM  `creature_summon_groups` WHERE `summonerId` = @ENTRY AND `summonerType` = 0;
+/*## Step 4: Creature Gossip Menu Options #############################*/
+
+/* Step 2.7 Table  `gossip_menu */
+DELETE FROM gossip_menu WHERE  `entry` = 0;
+/*## Step 5: Creature NPC Trainer / Vendor ############################*/
+
+/* Step 2.0 Table  `npc_trainer */
+DELETE FROM npc_trainer WHERE `entry` = @ENTRY;
+/* Step 1.9 Table  `npc_vendor */
+DELETE FROM npc_vendor WHERE  `entry` = @ENTRY;
+/*## Step 6: Creature NPC Conditions ##################################*/
+
+/* Step 6.9 Table `conditions` */
+/*DELETE FROM  `conditions` WHERE `SourceTypeOrReferenceId` AND `SourceGroup`;
+*/
+
+/*## Step 7: Creature NPC Needs to Work ###############################*/
+
+/* Step 2.2 Table  `creature_questrelation */
+DELETE FROM  `creature_questrelation` WHERE `id`= @ENTRY;
+/* Step 2.2 Table  `creature_involvedrelation */
+DELETE FROM  `creature_involvedrelation` WHERE `id`= @ENTRY;
+/* Step 1.8 Table  `waypoints */
+DELETE FROM waypoints WHERE  `entry` = @ENTRY;
+/* Step 2.5 Table  `npc_spellclick_spells */
+DELETE FROM npc_spellclick_spells WHERE  `npc_entry` = @ENTRY;
+/* Step 2.6 Table  `pet_levelstats */
+DELETE FROM pet_levelstats WHERE  `creature_entry` = @ENTRY;
+
